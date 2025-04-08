c27611c1a500129128d487938d2bee058a8f93c9
GouravKumarJatav <155358022+GouravKumarJatav@users.noreply.github.com>
2025-04-08 08:43:31 +0000

    Datatypes config done

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+console.log(3
+    +3)
+
+console.log("Gourav")
+
+// Data types 
+ let age = 21;
+ let isLoggedIn = false;
+ let state = null; 
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined
+ //symbol =>unique
+
+
+ //objest => null
+
+ console.log(typeof null);
+ 
