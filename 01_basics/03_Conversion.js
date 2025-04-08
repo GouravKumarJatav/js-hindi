5682cf9d346b2ced531cb6693f4af906439b3a00
GouravKumarJatav <155358022+GouravKumarJatav@users.noreply.github.com>
2025-04-08 16:30:48 +0000

    conversion of datatypes

---
+++ 01_basics/03_Conversion.js
@@ -0,0 +1,36 @@
+//    let score = "33abc"
+
+//    console.log(typeof score);
+//    console.log(typeof(score));
+
+//    let ValueInNumber = Number(score);
+//    console.log(typeof ValueInNumber);
+//    console.log(ValueInNumber);
+ 
+
+//    let score = null
+
+//    console.log(typeof score);
+//    console.log(typeof(score));
+
+//    let ValueInNumber = Number(score);
+//    console.log(typeof ValueInNumber);
+//    console.log(ValueInNumber);
+
+//   let score = undefined
+
+//    console.log(typeof score);
+//    console.log(typeof(score));
+
+//    let ValueInNumber = Number(score);
+//    console.log(typeof ValueInNumber);
+//    console.log(ValueInNumber);
+
+  let score = true 
+
+   console.log(typeof score);
+   console.log(typeof(score));
+
+   let ValueInNumber = Number(score);
+   console.log(typeof ValueInNumber);
+   console.log(ValueInNumber);
