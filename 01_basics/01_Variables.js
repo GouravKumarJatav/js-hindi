9cb842b37edeff03190a32b000b88be480fefa19
GouravKumarJatav <155358022+GouravKumarJatav@users.noreply.github.com>
2025-04-08 08:02:38 +0000

    Variables Configure done

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 134654
+let accountEmail = "Gourav@google.com"
+var accountPassword = "12345"
+accountCity = "Narsinghpur"
+
+
+//accountId = 2 //not allowed
+   accountCity = "jaipur"
+   accountEmail = "Gourav@google.com"
+   accountPassword = "12345"
+
+   console.table([accountEmail,accountPassword,accountCity]);
+   
